6d7f1e2d872b46234093b03dd41e4345d5f316fa
mapenda-source <mapendadioum3@gmail.com>
2022-12-15 20:19:18 -0500

    init projet

---
+++ architecture_netflix_uber/src/main/java/com/mapenda/architecture_netflix_uber/ArchitectureNetflixUberApplication.java
@@ -0,0 +1,13 @@
+package com.mapenda.architecture_netflix_uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureNetflixUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureNetflixUberApplication.class, args);
+	}
+
+}
---
+++ architecture_netflix_uber/src/test/java/com/mapenda/architecture_netflix_uber/ArchitectureNetflixUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapenda.architecture_netflix_uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureNetflixUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
